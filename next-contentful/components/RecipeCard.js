c873efd7d8a0a28c134b90dacaf4c0ef8bf3d490
Debangsu Das <debangsudas90@outlook.com>
2024-08-28 21:26:45 +0530

    lesson-5 adding images

--- next-contentful/components/RecipeCard.js
+++ next-contentful/components/RecipeCard.js
@@ -1,20 +1,25 @@
 import Link from "next/link"
+import Image from 'next/image'
 
 export default function RecipeCard({ recipe }) {
     const { title, slug, cookingTime, thumbnail } = recipe.fields
 
   return (
-    <div class="card">
-        <div class="featured">
-            {/* {image} */}
+    <div className="card">
+        <div className="featured">
+            <Image 
+                src={`http://${thumbnail.fields.file.url}`}
+                width={thumbnail.fields.file.details.image.width}
+                height={thumbnail.fields.file.details.image.height}
+            />
         </div>
-        <div class="content">
-            <div class="info">
+        <div className="content">
+            <div className="info">
                 <h4>{ title }</h4>
                 <p>Takes approx { cookingTime } mins to make</p>
             </div>
         </div>
-        <div class="actions">
+        <div className="actions">
             <Link href={`/recipes/${slug}`}>
                 Cook this
             </Link>
